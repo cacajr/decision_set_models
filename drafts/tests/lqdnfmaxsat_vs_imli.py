19e55d670f94dba6c03805d33cb1c008d47cf613
cacajr <realcarlos7@hotmail.com>
2023-04-11 02:53:43 -0300

    Feat: adding script for model tests

---
+++ drafts/tests/lqdnfmaxsat_vs_imli.py
@@ -0,0 +1,108 @@
+import sys, os
+if not sys.path[0] == os.path.abspath('...'):
+    sys.path.insert(0, os.path.abspath('...'))
+
+from models.imli import IMLI
+from models.lqdnfmaxsat import LQDNFMaxSAT
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+
+# training configurations
+database_name = 'iris'
+categorical_columns_index = []
+number_lines_per_partition = [8, 16]
+max_rule_set_sizes = [1, 2, 3]
+max_sizes_each_rule = [1, 2, 3]
+rules_accuracy_weights = [5, 10]
+number_realizations = 10
+database_path = f'./databases/{database_name}.csv'
+imli_results_path = f'./drafts/tests/lqdnfmaxsat_vs_imli_results/{database_name}_imli.csv'
+lqdnfmaxsat_results_path = f'./drafts/tests/lqdnfmaxsat_vs_imli_results/{database_name}_lqdnfmaxsat.csv'
+
+# import dataset
+Xy = pd.read_csv(database_path)
+X = Xy.drop(['Class'], axis=1)
+y = Xy['Class']
+
+# dataframes that will save the imli and lqdnfmaxsat results, respectively
+columns = ['Configuration', 'Rule set size', 'Larger rule size', 'Accuracy', 'Training time']
+imli_results_df = pd.DataFrame([], columns=columns)
+lqdnfmaxsat_results_df = pd.DataFrame([], columns=columns)
+
+for lpp in tqdm(number_lines_per_partition, desc='Number lines per partition'):
+    for mrss in tqdm(max_rule_set_sizes, desc='Maximum rule set size     '):
+        for raw in tqdm(rules_accuracy_weights, desc='Rule accuracy weight      '):
+            for r in tqdm(range(number_realizations), desc='Realization               '):
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+                imli_model = IMLI(
+                    max_rule_set_sizes = mrss,
+                    rules_accuracy_weights = raw,
+                    categorical_columns_index = categorical_columns_index,
+                    number_quantiles_ordinal_columns = 5,
+                    number_lines_per_partition = lpp,
+                    balance_instances = True    # TODO: see the balance method (seed)
+                )
+
+                imli_model.fit(X_train, y_train)
+
+                imli_result = pd.DataFrame([[
+                    f'lpp: {lpp} | mrss: {mrss} | raw: {raw}',
+                    imli_model.get_rule_set_size(),
+                    imli_model.get_larger_rule_size(),
+                    imli_model.score(X_test, y_test),
+                    imli_model.get_total_time_solver_solutions()    # TODO: see the time unit
+                ]], columns=columns)
+
+                pd.concat([imli_results_df, imli_result])
+            
+                for mser in tqdm(max_sizes_each_rule, desc='Maximum size each rule    '):
+                    lqdnfmaxsat_model = LQDNFMaxSAT(
+                        max_rule_set_sizes = mrss,
+                        max_sizes_each_rule = mser,
+                        rules_accuracy_weights = raw,
+                        categorical_columns_index = categorical_columns_index,
+                        number_quantiles_ordinal_columns = 5,
+                        number_lines_per_partition = lpp,
+                        balance_instances = True    # TODO: see the balance method (seed)
+                    )
+
+                    lqdnfmaxsat_model.fit(X_train, y_train)
+
+                    lqdnfmaxsat_result = pd.DataFrame([[
+                        f'lpp: {lpp} | mrss: {mrss} | raw: {raw}',
+                        lqdnfmaxsat_model.get_rule_set_size(),
+                        lqdnfmaxsat_model.get_larger_rule_size(),
+                        lqdnfmaxsat_model.score(X_test, y_test),
+                        lqdnfmaxsat_model.get_total_time_solver_solutions()    # TODO: see the time unit
+                    ]], columns=columns)
+
+                    pd.concat([lqdnfmaxsat_results_df, lqdnfmaxsat_result])
+
+            imli_averages = pd.DataFrame([[
+                'Averages',
+                imli_results_df['Rule set size'].iloc[-1: -number_realizations+1: -1].mean(),
+                imli_results_df['Larger rule size'].iloc[-1: -number_realizations+1: -1].mean(),
+                imli_results_df['Accuracy'].iloc[-1: -number_realizations+1: -1].mean(),
+                imli_results_df['Training time'].iloc[-1: -number_realizations+1: -1].mean()
+            ]], columns=columns)
+
+            pd.concat([imli_results_df, imli_averages])
+
+            lqdnfmaxsat_averages = pd.DataFrame([[
+                'Averages',
+                lqdnfmaxsat_results_df['Rule set size'].iloc[-1: -number_realizations*len(max_sizes_each_rule)+1: -1].mean(),
+                lqdnfmaxsat_results_df['Larger rule size'].iloc[-1: -number_realizations*len(max_sizes_each_rule)+1: -1].mean(),
+                lqdnfmaxsat_results_df['Accuracy'].iloc[-1: -number_realizations*len(max_sizes_each_rule)+1: -1].mean(),
+                lqdnfmaxsat_results_df['Training time'].iloc[-1: -number_realizations*len(max_sizes_each_rule)+1: -1].mean()
+            ]], columns=columns)
+
+            pd.concat([lqdnfmaxsat_results_df, lqdnfmaxsat_averages])
+
+# save results in csv file
+imli_results_df.to_csv(imli_results_path, index=False)
+lqdnfmaxsat_results_df.to_csv(lqdnfmaxsat_results_path, index=False)
